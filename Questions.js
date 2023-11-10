2916978ad8685de50f057fca205f2f0f0984e5b6
Luke1836 <codingluke2023@gmail.com>
2023-11-10 08:14:51 +0530

    Modified code

--- Questions.js
+++ Questions.js
@@ -1,37 +1,21 @@
-/* 
-const questions = document.getElementById('question');
-const minusBtn = document.querySelector('.minus');
-const plusBtn = document.querySelector('.plus');
-const ans = document.querySelector('.answer');
 
-plusBtn.addEventListener('click', () => {
-  plusBtn.style.display = 'none';
-  minusBtn.style.display = 'inline-block';
-  ans.style.display = 'block';
-});
-
-minusBtn.addEventListener('click', () => {
-  plusBtn.style.display = 'inline-block';
-  minusBtn.style.display = 'none';
-  ans.style.display = 'none';
-}); */
 const questions = document.querySelectorAll('.question');
 
 questions.forEach((question) => {
-  const plusBtn = question.querySelector('.plus');
-  const minusBtn = question.querySelector('.minus');
-  const answer = question.querySelector('.answer');
-
-  plusBtn.addEventListener('click', () => {
-    answer.style.display = 'block';
-    plusBtn.style.display = 'none';
-    minusBtn.style.display = 'inline-block';
-  });
-
-  minusBtn.addEventListener('click', () => {
-    answer.style.display = 'none';
-    plusBtn.style.display = 'inline-block';
-    minusBtn.style.display = 'none';
-  });
+  const btn = document.querySelector('.question-btn');
+  const ans = question.querySelector('.qn-ans');
+  btn.addEventListener('click', () => {
+    console.log("Luke");
+    questions.forEach((item) => {
+      if(item !== question)
+      {
+        item.classList.remove("active");
+      }
+    });
+    question.classList.toggle("active");
+  }) 
 });
+
+
+
 
